3fef68d375ad7e768dffa8a48701dc20724553d7
KevinSia <kevinsiakeemun@gmail.com>
2024-03-06 13:49:49 +0800

    Update

--- fillbucket.sh
+++ fillbucket.sh
@@ -36,6 +36,7 @@ curl "${CURL_OPTS[@]}" "https://api.bitbucket.org/2.0/repositories/$spacename/$r
 
 echo "Pushing to remote..."
 git remote add bitbucket https://"$username:$password"@bitbucket.org/$spacename/$reponame.git
+git fetch bitbucket
 git checkout -b newBranch bitbucket/master
 git log -10
 
