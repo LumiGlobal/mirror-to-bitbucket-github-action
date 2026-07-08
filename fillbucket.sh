9c860948616863dc0db804dace73fee71cdf6e19
KevinSia <kevinsiakeemun@gmail.com>
2026-07-08 18:19:58 +0800

    Test

--- fillbucket.sh
+++ fillbucket.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+urlencode() { local string="${1}"; local strlen=${#string}; local encoded=""; local pos c o; for (( pos=0 ; pos<strlen ; pos++ )); do c=${string:$pos:1}; case "$c" in [-_.~a-zA-Z0-9] ) o="${c}" ;; * ) printf -v o '%%%02X' "'$c" ;; esac; encoded+="${o}"; done; echo "${encoded}"; }
+
 set -o errexit
 set -o pipefail
 set -o nounset
@@ -17,7 +19,7 @@ fi
 
 
 CURL_OPTS=(-u "$username:$password" --silent)
-
+ENCODED_PASSWORD=$(urlencode $password)
 
 echo "Validating BitBucket credentials..."
 curl --fail "${CURL_OPTS[@]}" "https://api.bitbucket.org/2.0/user" > /dev/null || (
@@ -37,7 +39,7 @@ curl "${CURL_OPTS[@]}" "https://api.bitbucket.org/2.0/repositories/$spacename/$r
 echo $(git merge-base HEAD bitbucket/master)
 echo "Pushing to remote..."
 echo $(git rev-parse HEAD)
-git remote add bitbucket https://"$username:$password"@bitbucket.org/$spacename/$reponame.git
+git remote add bitbucket https://"$username:$ENCODED_PASSWORD"@bitbucket.org/$spacename/$reponame.git
 echo $(git branch)
 echo $(git rev-list --count bitbucket/master)
 echo $(git rev-list --count HEAD)
